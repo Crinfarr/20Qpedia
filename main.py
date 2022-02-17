2d2cea36b7643f74839a3883f21af32c566eb61e
Asher Hoeveler <contact@hecancanyou.xyz>
2022-02-17 15:23:13 -0600

    update a few things

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import wikipediaapi as wapi
 import discord
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import requests
 
+gamesInProgress = {}
+
+print('setting up NLP')
 model_name = "deepset/roberta-base-squad2"
+print('starting pipeline')
 nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 QA_input = {
     'question':'',
     'context':''
 }
+print('loading model')
 model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+print('loading tokenizer')
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
+print('DONE! Moving to bot setup...')
 client = discord.Client()
 
 @client.event
@@ -24,3 +30,17 @@ async def on_message(msg):
         # TODO: if game is in progress for author id, respond to any message containing a question mark
         # TODO: basically everything
         args.pop(0) # remove invocation statement probably
+        if args[0] == 'start':
+            gamesInProgress[msg.author.id] = next(iter(requests.get('https://en.wikipedia.org/w/api.php', {
+                'action':'query',
+                'generator':'random',
+                'rnnamespace':'0',
+                'prop':'extracts',
+                'format':'json',
+                'explaintext':1
+            }).json()['query']['pages'].values()))
+            print(str(gamesInProgress))
+            await msg.channel.send(str(gamesInProgress))
+
+
+client.run(open('token.txt', 'r').read())
